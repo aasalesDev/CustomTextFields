daf2592d9c83ed3273335367e88c4ea5ef10beb3
Anderson Sales <andersonsales.mg@gmail.com>
2023-02-19 13:38:34 -0300

    Create button programmatically

--- CustomTextFields/CustomTextFields/Controller/MainVC.swift
+++ CustomTextFields/CustomTextFields/Controller/MainVC.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var hourlyWageTF: CurrencyTextField!
+    @IBOutlet weak var itemPriceTF: CurrencyTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createUIButtonAndAddToKeyboard()
+    }
+    
+    private func createUIButtonAndAddToKeyboard() {
+        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 52))
+        calculateButton.backgroundColor = UIColor.systemOrange
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitleColor(UIColor.white, for: .normal)
+        calculateButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+        
+        hourlyWageTF.inputAccessoryView = calculateButton
+        itemPriceTF.inputAccessoryView = calculateButton
+        
+    }
+    
+    @objc private func calculate() {
+        print("Ho ho ho")
     }
 
 
