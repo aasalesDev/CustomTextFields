451762eab7236cc290cac795db1ce8e798aca1f0
Anderson Sales <andersonsales.mg@gmail.com>
2023-02-19 15:06:09 -0300

    Created button with trailing closure syntax

--- CustomTextFields/CustomTextFields/Controller/MainVC.swift
+++ CustomTextFields/CustomTextFields/Controller/MainVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
     @IBOutlet weak var hourlyWageTF: CurrencyTextField!
     @IBOutlet weak var itemPriceTF: CurrencyTextField!
     
@@ -19,21 +19,34 @@ class MainVC: UIViewController {
     }
     
     private func createUIButtonAndAddToKeyboard() {
-        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 52))
-        calculateButton.backgroundColor = UIColor.systemOrange
-        calculateButton.setTitle("Calculate", for: .normal)
-        calculateButton.setTitleColor(UIColor.white, for: .normal)
-        calculateButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+        //        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 52))
+        //        calculateButton.backgroundColor = UIColor.systemOrange
+        //        calculateButton.setTitle("Calculate", for: .normal)
+        //        calculateButton.setTitleColor(UIColor.white, for: .normal)
+        //        calculateButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+        //
+        //        /// This two lines attach the created button to the keyboard!
+        //        hourlyWageTF.inputAccessoryView = calculateButton
+        //        itemPriceTF.inputAccessoryView = calculateButton
         
-        hourlyWageTF.inputAccessoryView = calculateButton
-        itemPriceTF.inputAccessoryView = calculateButton
+        lazy var button: UIButton = {
+            let btn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 52))
+            btn.backgroundColor = UIColor.systemOrange
+            btn.setTitle("Calculate", for: .normal)
+            btn.setTitleColor(UIColor.white, for: .normal)
+            btn.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+            return btn
+        }()
+        
+        hourlyWageTF.inputAccessoryView = button
+        itemPriceTF.inputAccessoryView = button
         
     }
     
     @objc private func calculate() {
         print("Ho ho ho")
     }
-
-
+    
+    
 }
 
